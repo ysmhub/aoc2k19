09b868b8155ac3eedf7f1dff225c8859cc9aff64
hope <hope@syneforge.com>
2019-12-19 19:30:00 +0200

    pandas removed due to low performance

--- day06/d6p1.py
+++ day06/d6p1.py
@@ -1,9 +1,20 @@
 import sys
-import pandas as pd
 
+map = []
 with open(sys.argv[1]) as f:
-    df = pd.read_csv(f,sep=')',header=None)
-    planets = set(list(df[0]) + list(df[1]))
+    for line in f:
+        line = line.rstrip()
+        map.append(line.split(')'))
+    
+    print(map)
+    
+    planets = []
+    for list in map:
+        for planet in list:
+            planets.append(planet)
+
+    planets = set(planets)
+    print(set(planets))
     
     tk = 0
     for planet in planets:
@@ -11,15 +22,12 @@ with open(sys.argv[1]) as f:
         path = planet
         k = 0
         while planet != sun:
-            for x in range(0,len(df)):
-                if df.loc[x,1] == planet:
-                    planet = df.loc[x,0]
+            for x in map:
+                if x[1] == planet:
+                    planet = x[0]
                     path += f"-{planet}"
                     k = k + 1
                     break
         print (f"{path},({k})")
         tk = tk + k
     print(f"total number of direct and indirect orbits: {tk}")
-
-
-
